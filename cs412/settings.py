a019856b510b1fe6ba388a001babcacbbdd1af28
Donovan <dbeyer03@bu.edu>
2024-09-29 22:26:46 -0400

    trying to add whitenoise

--- cs412/settings.py
+++ cs412/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "whitenoise.runserver_nostatic",
     'restaurant',
     'quotes', ## quotes app for assign 3!
 ]
@@ -129,6 +130,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
