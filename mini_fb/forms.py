d1615fb8736d4ba59475ad0703af45d321ba5457
Donovan <dbeyer03@bu.edu>
2024-10-13 15:11:36 -0400

    commented code

--- mini_fb/forms.py
+++ mini_fb/forms.py
@@ -7,7 +7,7 @@ class CreateProfileForm(forms.ModelForm):
   last_name = forms.CharField(label="Last Name", required=True)
   city = forms.CharField(label="City", required=True)
   email_address = forms.CharField(label="Email Address", required=True)
-  profile_image_url = forms.CharField(label="Profile Image", required=True)
+  profile_image_url = forms.URLField(max_length=10000, label="Profile Image", required=True, widget=forms.TextInput(attrs={'placeholder': 'No URLs greater than 200 characters.', 'style': 'min-width: 150px; max-width: 500px; width: 190%;'}))
 
 
   class Meta:
@@ -15,7 +15,8 @@ class CreateProfileForm(forms.ModelForm):
     fields = ['first_name','last_name','city','email_address','profile_image_url']
 
 class CreateStatusMessageForm(forms.ModelForm):
-  text = forms.CharField(label="Status Message", required=True)
+  '''A form to create Status Message forms.'''
+  text = forms.CharField(label="Status Message", required=True, widget=forms.Textarea(attrs={'placeholder': 'Status Message', 'style': 'width: 90%; max-width: 900px; height: 150px;;'}))
 
   class Meta:
     model = StatusMessage 
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -30,26 +30,30 @@ def info_page(request):
   return render(request,template_name,context)
 
 class ShowProfilePageView(DetailView):
+  '''A view to show an individual profile page.'''
   model = Profile 
   template_name = 'mini_fb/show_profile.html'
   context_object_name = 'profile'
 
 
 class CreateProfileView(CreateView):
+  '''A view to create a new profile page.'''
   form_class = CreateProfileForm
   template_name = 'mini_fb/create_profile_form.html'
 
 class CreateStatusMessageView(CreateView):
+  '''A view to create a status message on a profile page.'''
   form_class = CreateStatusMessageForm 
   template_name = 'mini_fb/create_status_form.html'
 
   def get_context_data(self, **kwargs: any) -> dict[str,any]:
 
+    # identify profile by its kwargs.
     context = super().get_context_data(**kwargs)
-
     profile = Profile.objects.get(pk=self.kwargs['pk'])
-
+    # store kwargs info in dictionary.
     context['profile'] = profile 
+
     return context
 
   def form_valid(self,form):
@@ -67,7 +71,6 @@ class CreateStatusMessageView(CreateView):
     return super().form_valid(form)
 
   def get_success_url(self) -> str: 
-
     #redirects the URL on success.
     profile = Profile.objects.get(pk=self.kwargs['pk'])
     return reverse('show_profile',kwargs={'pk':profile.pk})
